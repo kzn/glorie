a9386a5c353619ed472d9cb5877f664bf2568095
Anton Kazennikov <kazennikov@gmail.com>
2018-07-13 12:46:57 +0300

    javadocs

--- src/main/java/name/kazennikov/glorie/ProductionRewriter.java
+++ src/main/java/name/kazennikov/glorie/ProductionRewriter.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * GLR grammar production rewriter.
  *
- * The rewriters are used to transform a grammar from EBNF to BNF
+ * The rewriters help to transform a grammar from EBNF to BNF
  */
 public interface ProductionRewriter {
 	public List<Production> rewriter(Grammar g, Production p);
